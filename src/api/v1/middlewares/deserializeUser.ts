e8bde9bf88e2ab9c7cd501a2df2f742c6a115ee8
EmericBayard <57359277+EmericBayard@users.noreply.github.com>
2021-10-06 11:55:51 +0200

    🐛 FIX: deserializeUser

--- src/api/v1/middlewares/deserializeUser.ts
+++ src/api/v1/middlewares/deserializeUser.ts
@@ -2,16 +2,16 @@ import { get } from "lodash";
 import { Request, Response, NextFunction } from "express";
 import { decode } from "../helpers/JwtUtils";
 import { reIssueAccessToken } from "../services/SessionService"
-
+import log from "../helpers/logger";
 const deserializeUser = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
+    
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
 
     const refreshToken = get(req, "headers.x-refresh");
-
     if(!accessToken) return next();
 
     const { decoded, expired } = decode(accessToken);
--- src/api/v1/middlewares/requiresUser.ts
+++ src/api/v1/middlewares/requiresUser.ts
@@ -1,13 +1,12 @@
 import { get } from "lodash";
 import { Request, Response, NextFunction } from "express";
-
+import log from "../helpers/logger";
 const requiresUser = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
     const user = get(req, "user");
-
     if(!user) {
         return res.sendStatus(403);
     }
